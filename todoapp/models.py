337f92fea7f7a527301a6e4e0e5318e9ac1e9fa9
Alex <asmyshlyaev177@gmail.com>
2017-05-05 19:48:44 +0300

    models

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=140)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.DateTimeField(blank=True, null=True)
+    
+    class Meta:
+        abstract = True
+
+    
+class TodoList(Todo):
+    pass
+    
+    
+class Task(Todo):
+    order = models.PositiveSmallIntegerField(blank=True, null=True)
+    todo = models.ForeignKey('TodoList')
