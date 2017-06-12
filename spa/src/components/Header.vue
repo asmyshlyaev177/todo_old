9aa707f908ee1e0215c06e7a43a08a193652714c
Asmyshlyaev177 <asmyshlyaev177@gmail.com>
2017-06-12 11:48:45 +0000

    small fixes and nginx config

--- spa/src/components/Header.vue
+++ spa/src/components/Header.vue
@@ -6,7 +6,7 @@
         Todo App
       </h1>
       <h2 class="subtitle has-text-centered">
-        Made with Vue.js and bulma.io
+        Made with Vue.js, vuex, vue-router and bulma.io
       </h2>
     </div>
   </div>
--- spa/src/main.js
+++ spa/src/main.js
@@ -13,7 +13,7 @@ Vue.use(Vuex);
 Vue.use(VueRouter);
 import bus from './components/bus';
     
-var host = '192.168.1.70:8080';
+var host = 'todo.asmyshlyaev177.tk';
 
 Vue.directive('focus', {
   inserted: function (el) {
